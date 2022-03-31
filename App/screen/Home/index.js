e01e92c53a2389a922c5d70e85bf5d01991bb6b2
waqas <mwaqaskhanroghnai@gmail.com>
2022-03-31 02:56:46 -0700

    add splash screen feature

--- App/screen/Home/index.js
+++ App/screen/Home/index.js
@@ -2,10 +2,12 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import styles from './styles';
 import {WebView} from 'react-native-webview';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function Home() {
   const google = 'http://www.google.com';
   const portal = 'https://www.awkumlms.com/Student/index';
+
   return (
     <View style={{backgroundColor: '#2E9C4C'}}>
       {/* <Text style={styles.mainText}>AWKUM LMS APP </Text> */}
